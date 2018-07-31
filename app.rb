794ce088a6c0fa8658e2b4dd009bbc4c8e6417a8
12helenl <12helenl@gmail.com>
2018-07-31 18:36:07 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,3 +2,16 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
 end
+
+get'/name'do
+  "My Name is _"
+end
+
+get'/hometown'do
+  "My hometown is_"
+end
+
+get'/favorite-song'do
+  "My favorite-song"
+end
+
